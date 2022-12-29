544bda277d719667ca7d94a2a1ee15426fe0e495
Csordas Balazs <csordasbalu96@gmail.com>
2022-12-29 08:43:44 +0100

    test2

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from 'next/server'
 
 const authMiddleware = (req: NextRequest) => {
-    const jwt = req.cookies.get('jwt')?.value;
-    console.log(req.cookies);
-    let url = req.url;
 
-    if(!jwt && url.includes('/admin')){
-        return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/login`)
-    }
 }
 
 export default authMiddleware;
