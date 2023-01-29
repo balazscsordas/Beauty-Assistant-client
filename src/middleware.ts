a10524fa4a9e06437a2f621848fd7b84401e84b8
Csordas Balazs <csordasbalu96@gmail.com>
2023-01-29 20:41:59 +0100

    cookie test 15

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,13 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from 'next/server'
 
 const authMiddleware = (req: NextRequest ) => {
-    /* const jwt = req.cookies.get('jwt')?.value;
+    const jwt = req.cookies.get('jwt')?.value;
+    const res = NextResponse.next()
     let url = req.url;
 
     if(!jwt && url.includes('/admin')){
         return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/`)
-    } */
+    }
 }
 
 export default authMiddleware;
