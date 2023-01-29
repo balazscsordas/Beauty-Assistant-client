0a1589520d1df35c2418a4ef893b2210c735811f
Csordas Balazs <csordasbalu96@gmail.com>
2023-01-29 12:14:18 +0100

    removed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from 'next/server'
 
 const authMiddleware = (req: NextRequest ) => {
-    const jwt = req.cookies.get('jwt')?.value;
+    /* const jwt = req.cookies.get('jwt')?.value;
     let url = req.url;
 
     if(!jwt && url.includes('/admin')){
         return NextResponse.redirect(`${process.env.NEXT_PUBLIC_BASE_URL}/`)
-    }
+    } */
 }
 
 export default authMiddleware;
