e3437cb41e951adfad75b66ceb7a2bef1ec55cc1
Csordas Balazs <csordasbalu96@gmail.com>
2023-02-02 22:20:23 +0100

    added some new text

--- src/components/homepage/MainBanner.tsx
+++ src/components/homepage/MainBanner.tsx
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import AuthContext from "../../context/AuthProvider";
 import LangContext from "../../context/LanguageProvider";
 import { BasicPrimaryButton } from "../smallComponents/Buttons";
+import cubes from "../../../public/cubes.png"
 
 const MainBanner = () => {
 
@@ -35,9 +36,9 @@ const MainBanner = () => {
     return (
         <section className="flex justify-center items-center min-h-50vh bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-sky-600 to-indigo-900">
             <div className="text-center text-white">
-                <h1 className="mb-4 text-3xl sm:text-5xl sm:mb-6 font-bold">Beauty Assistant</h1>
-                <p>{lang === 'hun' ? 'Gyorsan, egyszerűen, hatékonyan.' : 'Fast, quick, effective'}</p>
-                <div className="flex justify-center max-w-lg m-auto mt-4">
+                <h1 className="mb-4 text-3xl sm:text-5xl sm:mb-10 font-bold">Beauty Assistant</h1>
+                <p>{lang === 'hun' ? 'Időpontkezelés, vendég nyilvántartás gyorsan, egyszerűen, hatékonyan.' : 'Fast, quick, effective'}</p>
+                <div className="flex justify-center max-w-lg m-auto mt-6">
                     <Link className="m-2" passHref href="/registration">
                         <BasicPrimaryButton text={ lang === 'hun' ? 'Regisztráció' : 'Registration' }/>
                     </Link>
