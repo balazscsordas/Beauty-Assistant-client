fe1c9bf12a721bbb3a8c1604baa67e954b75b5f6
Csordas Balazs <csordasbalu96@gmail.com>
2023-02-03 21:02:32 +0100

    style changes in statistics

--- src/components/statistics/StatWrapper.tsx
+++ src/components/statistics/StatWrapper.tsx
@@ -7,7 +7,7 @@ type Props = {
 const StatWrapper = ({ children }: Props) => {
 
     return (
-        <section className="bg-white p-6 rounded-xl my-4 shadow-md m-auto flex-1 xl:mx-6">
+        <section className="bg-white p-6 rounded-xl my-4 shadow-md m-auto flex-1 xl:mx-6 w-full">
             {children}
         </section>
     )
